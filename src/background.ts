155ce72cb3cfaf35e83619caccada1eb68cb8c52
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-04-26 15:57:18 +0800

    Minimize permissions

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,11 @@
 import { IS_FIREFOX } from "./util/constant/environment"
 
-chrome.browserAction.onClicked.addListener((_tab: chrome.tabs.Tab) => {
+function openAppPage() {
     // FireFox use 'static' as prefix
     const url = IS_FIREFOX ? 'app.html' : 'static/app.html'
     chrome.tabs.create({ url })
-})
+}
+
+chrome.browserAction.onClicked.addListener(openAppPage)
+
+chrome.runtime.onInstalled.addListener(details => details.reason === 'install' && openAppPage())
--- src/manifest.js
+++ src/manifest.js
@@ -38,7 +38,6 @@ module.exports = {
         }
     ],
     permissions: [
-        'storage',
-        'tabs',
+        'storage'
     ]
 }
