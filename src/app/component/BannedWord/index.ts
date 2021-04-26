2bf271427c4ca22821dc88e068e0a1b74df99d73
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-04-26 15:18:06 +0800

    Feedback link

--- src/app/component/BannedWord/index.ts
+++ src/app/component/BannedWord/index.ts
@@ -1,4 +1,4 @@
-import { ElButton } from 'element-plus'
+import { ElButton, ElLink, ElSpace } from 'element-plus'
 import { defineComponent, h, reactive, Ref, ref } from 'vue'
 import ListTable from './list-table'
 import DictEdit from './dict-edit'
@@ -29,8 +29,12 @@ export default defineComponent({
       { default: () => '新增词典' }
     )
 
+    const mailto = 'returnzhy1996@outlook.com'
+
+    const feedbackLink = h(ElLink, { icon: "el-icon-edit-outline", href: `mailto:${mailto}?subject=扩展使用反馈`, target: '_blank' }, () => '使用反馈')
+
     const children = [
-      h('div', { class: 'filter-container' }, [addButton]),
+      h('div', { class: 'filter-container' }, h(ElSpace, { size: 'large' }, () => [addButton, feedbackLink])),
       h(ListTable, { ref: 'table', onEdit: (row: XGFLFG.Dictionary) => { _ctx.$refs.edit.edit(row) } }),
       h(DictEdit, {
         ref: 'edit',
