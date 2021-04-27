a19633ce0ccda564ed0bdf11eb47550311d8dce9
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-04-27 15:09:50 +0800

    Replace the title

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,19 +1,14 @@
 import XGFLFG from '.'
-import replace from './replace/replacer'
+import replace, { replaceWithWords } from './replace/replacer'
 import { DictionaryService } from './service/dictionary-service'
 
 const service = new DictionaryService(chrome.storage.local)
 
 const config: MutationObserverInit = { attributes: false, childList: true, subtree: true }
 
-const generateDocumentObserver = (words: XGFLFG.BannedWord[]) => {
-    const regWords: XGFLFG.BannedWordUseReg[] = words
-        .map(word => {
-            return { origin: new RegExp(word.origin.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')), mask: word.mask }
-        }
-        )
+const generateDocumentObserver = (words: XGFLFG.BannedWordUseReg[]) => {
     return new MutationObserver((records: MutationRecord[]) =>
-        records.forEach(record => record.addedNodes.forEach(node => replace(node, regWords)))
+        records.forEach(record => record.addedNodes.forEach(node => replace(node, words)))
     )
 }
 
@@ -22,11 +17,17 @@ const href = window.location.href
 
 service.listWordsBy(host, href, words => {
     if (words.length) {
-        const observer = generateDocumentObserver(words)
+
+        const regWords: XGFLFG.BannedWordUseReg[] = words
+            .map(word => {
+                return { origin: new RegExp(word.origin.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')), mask: word.mask }
+            })
+
+        document.title = replaceWithWords(document.title, regWords)
+        const observer = generateDocumentObserver(regWords)
+
         observer.observe(document, config)
         window.onunload = observer.disconnect
         console.log('根据相关法律法规，将审核并替换敏感词')
     }
 })
-
-window.onload = () => console.log(document.title)
