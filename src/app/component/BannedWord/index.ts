426c6d18881f45a491e64d53d241015ae0bbcb6c
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-05-26 23:49:54 +0800

    No options

--- src/app/component/BannedWord/index.ts
+++ src/app/component/BannedWord/index.ts
@@ -1,101 +1,93 @@
 import { ElButton, ElLink, ElMessage, ElSpace } from 'element-plus'
-import { defineComponent, h, reactive, Ref, ref } from 'vue'
+import { defineComponent, h, Ref, ref } from 'vue'
 import ListTable from './list-table'
 import DictEdit from './dict-edit'
 import XGFLFG from '../../..'
 import { checkJSON } from '../../../util/file-util'
 import DictionaryDb from '../../../database/dictionary-db'
 
-interface Props {
-  rows: XGFLFG.BannedWord[]
-  dialog: Ref
-}
+// My email
+const mailto = 'returnzhy1996@outlook.com'
 
 const db: DictionaryDb = new DictionaryDb(chrome.storage.local)
 
-const fileInputRef = 'file-import-input'
+// Ref of data edit dialog
+const editRef = ref()
+// Ref of file input
+const fileInputRef = ref() as Ref<HTMLInputElement>
+// Ref of table
+const tableRef = ref()
 
-export default defineComponent({
-  setup() {
-    const props: Props = {
-      rows: [],
-      dialog: ref()
-    }
-    return reactive(props)
-  },
-  methods: {
-    handleFileSelected() {
-      const files: FileList | null = (this.$refs[fileInputRef] as HTMLInputElement).files
-      if (!files || !files.length) {
-        return
-      }
-      const file: File = files[0]
-      const _this: any = this
-      file.text()
-        .then(str => checkJSON(str))
-        .then(dict => db.import(dict))
-        .then(() => {
-          ElMessage.success('成功导入')
-          _this.$refs.table.query()
-        })
-        .catch(ElMessage.error)
-    }
-  },
-  render(_ctx: any) {
-    const addButton = () => h(
-      ElButton,
-      {
-        size: 'small',
-        type: 'primary',
-        icon: 'el-icon-plus',
-        onClick: () => _ctx.$refs.edit.add()
-      },
-      { default: () => '新增词典' }
-    )
-
-    const fileInput = h('input', { ref: fileInputRef, type: 'file', accept: '.json', style: { display: 'none' }, onChange: _ctx.handleFileSelected })
-
-    const importButton = () => h(
-      ElButton,
-      {
-        size: 'small',
-        type: 'primary',
-        icon: 'el-icon-document-copy',
-        onClick: () => { _ctx.$refs[fileInputRef].click() }
-      },
-      { default: () => ['导入词典', fileInput] }
-    )
-
-    const mailto = 'returnzhy1996@outlook.com'
-
-    const feedbackLink = () => h(ElLink,
-      {
-        icon: "el-icon-edit-outline",
-        href: `mailto:${mailto}?subject=扩展使用反馈`,
-        target: '_blank'
-      },
-      () => '使用反馈'
-    )
-
-    const children = [
-      h(
-        'div',
-        { class: 'filter-container' },
-        h(
-          ElSpace,
-          { size: 'large' },
-          () => [addButton(), importButton(), feedbackLink()]
-        )
-      ),
-      h(ListTable, { ref: 'table', onEdit: (row: XGFLFG.Dictionary) => { _ctx.$refs.edit.edit(row) } }),
-      h(DictEdit, {
-        ref: 'edit',
-        onSaved: () => _ctx.$refs.table.query()
-      })
-    ]
-
-    const div = h('div', { class: 'app-container' }, children)
-
-    return div
+// Handle after the file to import is selected
+function handleFileSelected() {
+  const files: FileList = fileInputRef.value.files
+  if (!files.length) {
+    return
   }
+  const file: File = files[0]
+  file.text()
+    .then(str => checkJSON(str))
+    .then(dict => db.import(dict))
+    .then(() => {
+      ElMessage.success('成功导入')
+      tableRef.value.query()
+    })
+    .catch(ElMessage.error)
+}
+
+export default defineComponent(() => {
+
+
+  const addButton = () => h(
+    ElButton,
+    {
+      size: 'small',
+      type: 'primary',
+      icon: 'el-icon-plus',
+      onClick: () => editRef.value.add()
+    },
+    { default: () => '新增词典' }
+  )
+
+  // Input with file type, which hide in the import button
+  const fileInput = () => h('input', { ref: fileInputRef, type: 'file', accept: '.json', style: { display: 'none' }, onChange: handleFileSelected })
+
+  const importButton = () => h(
+    ElButton,
+    {
+      size: 'small',
+      type: 'primary',
+      icon: 'el-icon-document-copy',
+      onClick: () => fileInputRef.value.click()
+    },
+    { default: () => ['导入词典', fileInput()] }
+  )
+
+  const feedbackLink = () => h(ElLink,
+    {
+      icon: "el-icon-edit-outline",
+      href: `mailto:${mailto}?subject=扩展使用反馈`,
+      target: '_blank'
+    },
+    () => '使用反馈'
+  )
+
+  const children = () => [
+    h(
+      'div',
+      { class: 'filter-container' },
+      h(
+        ElSpace,
+        { size: 'large' },
+        () => [addButton(), importButton(), feedbackLink()]
+      )
+    ),
+    h(ListTable, { ref: tableRef, onEdit: (row: XGFLFG.Dictionary) => editRef.value.edit(row) }),
+    h(DictEdit, {
+      ref: editRef,
+      onSaved: () => tableRef.value.query()
+    })
+  ]
+
+  return () => h('div', { class: 'app-container' }, children())
 })
